4765704110d394328876f598c9fe0b166cb9bae7
Scot Matson <sjpmatson@gmail.com>
2017-10-19 22:39:25 -0700

    Made language specific sub directories. Also finally got merge sort working... with help. boy am I out of practice

---
+++ algorithms/sorting/Java/InsertionSortV1.java
@@ -0,0 +1,29 @@
+
+import java.util.Random;
+import java.util.Arrays;
+
+
+public class InsertionSortV1 {
+
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int[] nums = new int[20];
+
+        for (int i = 0; i < nums.length; i++) {
+          nums[i] = rand.nextInt(101);
+        }
+
+        System.out.println("Unsorted: " + Arrays.toString(nums));
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (nums[j-1] > nums[j]) {
+                    int temp = nums[j-1];
+                    nums[j-1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted: " + Arrays.toString(nums));
+    }
+}
--- algorithms/sorting/Java/InsertionSortV2.java
+++ algorithms/sorting/Java/InsertionSortV2.java
@@ -3,7 +3,7 @@ import java.util.Random;
 import java.util.Arrays;
 
 
-public class InsertionSort {
+public class InsertionSortV2 {
 
     public static void main(String[] args) {
 
@@ -16,11 +16,11 @@ public class InsertionSort {
 
         System.out.println("Unsorted: " + Arrays.toString(nums));
         for (int i = 1; i < nums.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if (nums[j-1] > nums[j]) {
-                    int temp = nums[j-1];
-                    nums[j-1] = nums[j];
-                    nums[j] = temp;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] < nums[j]) {
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
                 }
             }
         }
